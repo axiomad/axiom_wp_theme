a6a5dcc3405653c2f76caa81c865a51e40d79856
AXIOM Marketing <113052804+axiomad@users.noreply.github.com>
2023-01-19 16:08:08 -0600

    Create page.php

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php wp_head(); ?>
+<body id="body" <?php body_class(); ?>>
+<div id="site-container" class="site-container">
+<?php get_header(); ?>
+<?php if (have_posts()): ?>
+
+		<main id="main" class="site-main" role="main">
+			<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
+								the_content();
+				// End of the loop.
+				endwhile;
+			?>
+		</main>
+
+<?php endif; ?>
+<?php get_footer(); ?>
+</div>
